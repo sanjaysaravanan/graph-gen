24145e0a7963521384c46aa5e4351a9906205e85
sanjaysaravanan <sanjaysaravanan38@gmail.com>
2021-09-05 16:13:23 +0530

    added logic to parse the xls

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const onSubmit = (e) => {
+    e.preventDefault();
+    let data = Array.from(e.target.elements)
+      .filter((f) => f.nodeName === "INPUT")
+      .reduce(
+        (ac, a) => (a.name !== "" ? { ...ac, [a.name]: a.value } : { ...ac }),
+        {}
+      );
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2 style={{ lineHeight: "0px" }}>Generate Graphs</h2>
+        <h3>Upload Your XLS file to see the axis fields</h3>
+        <form onSubmit={onSubmit} noValidate>
+          <input
+            type="text"
+            id="myName"
+            placeholder="Enter a name"
+            name="name"
+          />
+          <br />
+          <input type="file" id="myFile" name="filename" />
+          <br />
+          <input type="submit" />
+        </form>
       </header>
     </div>
   );
